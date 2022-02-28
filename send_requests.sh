dc31d6dbf0c8d7e087b82559e8b5d61579b97678
Mr Nobody <mr@nobody>
2022-02-27 18:55:59 -0800

    added rt.ru

--- send_requests.sh
+++ send_requests.sh
@@ -5,5 +5,6 @@ do
 echo "sending requests"
 curl -s --max-time 1 https://www.pravda.ru/  > /dev/null
 curl -s --max-time 1 http://government.ru/  > /dev/null
+curl -s --max-time 1 https://www.rt.ru/  > /dev/null
 sleep 1.5
 done
